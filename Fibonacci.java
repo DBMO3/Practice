388d218436e5be9e8cae4f707fa081350aea6824
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-04-03 17:58:56 -0600

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int n, fibonacci = 0, F0 = 0, F1 = 1;
+		int n, F2 = 0, F0 = 0, F1 = 1;
 		n = in.nextInt();
 		
 		for(int i = 0; i <= n; i++){
 			System.out.println("F" + i + ": " + F0);
-			fibonacci = F1 + F0;
+			F2 = F1 + F0;
 			F0 = F1;
-			F1 = fibonacci;
+			F1 = F2;
 		}
 	}
 }
