a5790a1d2677c7cb85eee03d3047b805d8bd2441
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-04-03 15:33:41 -0600

    Create EvenNumber.java

---
+++ EvenNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n;
+		
+		n = in.nextInt();
+		if(n%2 == 0){
+			System.out.println("EVEN NUMBER");
+		} else {
+			System.out.println("NOT EVEN NUMBER");
+		}
+	}
+}
