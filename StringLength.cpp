87ab36a36320abcc130bc92537fbdf8c93152a03
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-04-03 19:05:58 -0600

    Create StringLength.cpp

---
+++ StringLength.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+	string word = "Hello word";
+
+	cout << word.length() << endl;
+	return 0;
+}
